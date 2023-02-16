aefc11b006487eca4f0a6677f4209eb95792166c
x-1 <x@g.co.za>
2023-02-16 10:38:44 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * Main prints > "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/*Syntax_puts: puts - output of characters*/
+int puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
