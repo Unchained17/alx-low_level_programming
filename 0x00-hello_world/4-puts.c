a35c7666426733baf717cfa33d1dba71f24bb2d2
x-1 <x@g.co.za>
2023-02-16 11:33:50 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-
-int puts(..."Programming is like building a multilingual puzzle");
+int putchar(int c);
+int puts("Programming is like building a multilingual puzzle");
 return (0);
 }
