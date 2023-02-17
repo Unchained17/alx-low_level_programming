a6253840129357ca89fa35a2d74861f73721decf
x-1 <x@g.co.za>
2023-02-17 04:41:13 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ * print text in quotes
  * return: 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	main return (0);
+	main; return (0);
 }
