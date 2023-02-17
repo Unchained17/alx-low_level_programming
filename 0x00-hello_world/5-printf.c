a6bac6bd2a3687b9f7515360436d6a00ecdb280b
x-1 <x@g.co.za>
2023-02-17 03:16:53 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * print exactly with proper grammar, but the outcome is a piece of art, as output 
+ * Description: print  with proper grammar but the outcome is a piece of art
  * return: 0 (success)
  */
 int main(void)
