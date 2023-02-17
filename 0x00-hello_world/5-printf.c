ed02c125eeda6ee6214f46f525248083eb59e2e6
x-1 <x@g.co.za>
2023-02-17 10:04:17 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,8 @@
  * main - Entry
  * return: 0
  */
-int main(){
-	printf("with proper grammar, but the outcome is a piece of art,");
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
 }
