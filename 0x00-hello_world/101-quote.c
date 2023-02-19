a317da74eb497a6cabfab7df1a8f828d42d4cf44
x-1 <x@g.co.za>
2023-02-19 09:52:15 -0800

    8. UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - The main function entry point
  *
- * Return: (0) for success
+ * Return: (1) for success
  */
 
-char main(void)
+int main(void)
 {
 	printf("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
