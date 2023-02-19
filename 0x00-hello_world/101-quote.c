f4580b9caf5a702e828b2d87a489af8913a8e34f
x-1 <x@g.co.za>
2023-02-19 09:50:42 -0800

    8. UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  * Return: (0) for success
  */
 
-int main(void)
+char main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
 	return (0);
 }
