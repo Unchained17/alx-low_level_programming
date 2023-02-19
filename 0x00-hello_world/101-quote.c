d0a91e61f5195287c81a852d92d199e8326e1094
x-1 <x@g.co.za>
2023-02-19 09:35:07 -0800

    8. UNIX

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - The main function entry point
+ *
+ * Return: (0) for success
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\"\t Dora Korpar, 2015-10-19\n");
+	return (0);
+}
