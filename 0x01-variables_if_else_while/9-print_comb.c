a81a05cde6fe1052866f2d8c97bfd5db66f8d5b7
x-1 <x@g.co.za>
2023-02-20 17:33:14 -0800

    9. Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: (0)
+ */
+int main(void)
+{
+	int y;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+	putchar(y);
+	if (y != '9')
+	{
+	putchar(',');
+	}
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
