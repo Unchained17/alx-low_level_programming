de099217ba8ee66fbf6883f9039f537928fc828c
x-1 <x@g.co.za>
2023-02-20 16:28:03 -0800

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: (0)
+ */
+int main(void)
+{
+	int w;
+
+	for (w = '0'; w <= '9'; w++)
+	putchar(w);
+	putchar('\n');
+	return (0);
+}
