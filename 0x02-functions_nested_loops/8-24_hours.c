c3835f75d6d09b24a1d5d28a9f2206c9850301db
x-1 <x@g.co.za>
2023-02-26 13:55:21 -0800

    8. federal agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59i
+ * jack_bauer - every minute of the day from 00:00 to 23:59
  * Return: (void)
  */
 void jack_bauer(void)
