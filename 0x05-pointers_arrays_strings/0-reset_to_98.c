3de586b23ed1c11614564538f89ac7011d1146e3
x-1 <x@g.co.za>
2023-03-04 10:36:14 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - function takes pointer to int(parameter), updates value to: 98
- * @*n: Pointer to parameter n 
+ * @n: Pointer to int argument n
  * Return: (void)
  */
 void reset_to_98(int *n)
