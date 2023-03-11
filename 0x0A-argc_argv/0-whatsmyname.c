04847dc909004c30b76f7ca30529692592c1fa5d
x-1 <x@g.co.za>
2023-03-11 05:48:50 -0800

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program that prints its name, followed by a new line.
+ * If you rename the program, it will print the new name,
+ * without having to compile it again
+ * @argv: an array of pointers to the strings which are 
+ * those arguments- (type) is (almost) ‘array of pointer to char’.
+ * @argc: a count of the arguments supplied to the program.
+ * Return: (0)
+ */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
