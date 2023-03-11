aa167baadcb6c4a0fdddfc1d195cd9ecb4fda6b6
x-1 <x@g.co.za>
2023-03-11 11:48:01 -0800

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - program that adds positive numbers.
+ * @argc: argument count
+ * @argv: vector
+ * Return: (0)
+ */
+int main(int argc, char **argv)
+{
+	int i, x, sum = 0;
+	char *p;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; argv[i]; i++)
+	{
+		x = strtol(argv[i], &p, 10);
+		if (*p)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += x;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
