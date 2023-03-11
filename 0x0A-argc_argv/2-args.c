0868de0b54aa1f5229bb8fa44e23507886ed2c28
x-1 <x@g.co.za>
2023-03-11 07:14:08 -0800

    2. The best argument against democracy

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,12 +10,7 @@ int main(int argc, char **argv)
 {
 	int count;
 
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-		{
-			printf("%s\n", argv[count]);
-		}
-	}
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[count]);
 	return (0);
 }
