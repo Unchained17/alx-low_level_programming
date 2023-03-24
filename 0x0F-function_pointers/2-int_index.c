6300ca3ba2ef3db362fbd5a7332d907b6bbbe25b
x-1 <x@g.co.za>
2023-03-24 08:26:05 -0700

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer.
+ * @size: number of elements in the array.
+ * @cmp: pointer to the function to be used to compare values.
+ * @array: array of integers.
+ * Return: (0)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x;
+
+	if (array == 0 || size <= 0 || cmp == 0)
+	{
+		return (-1);
+	}
+	x = 0;
+	while (x < size)
+	{
+		x++;
+		if ((*cmp)(*(array + x)))
+			return (x);
+	}
+	return (0);
+}
