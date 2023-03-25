ca5d52edffa46e07de775ba75e5ce69bd3e2d98d
x-1 <x@g.co.za>
2023-03-25 11:28:00 -0700

    3. A goal is something to aim at

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * main - Prints reslut of sample operations.
+ * @argc: number of arguments
+ * @argv: Vector/one-dimensional-array
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *po;
+
+	if (argc > 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	po = argv[2];
+	num2 = atoi(argv[3]);
+	if ((get_op_func(po) == NULL) || (po[1] != '\0'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*po == '/' && num2 == 0) || (*po == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(po)(num1, num2));
+	return (0);
+}
