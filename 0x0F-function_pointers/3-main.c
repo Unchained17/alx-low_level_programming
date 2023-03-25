de418111c3dc4f7bc06adf494a7fa1730aba0d1a
x-1 <x@g.co.za>
2023-03-25 11:42:51 -0700

    3. A goal serves as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 /**
  * main - Prints reslut of sample operations.
- * @argc: number of arguments
- * @argv: Vector/one-dimensional-array
+ * @argc: number of arguments.
+ * @argv: array of pointers to the arguments.
  * Return: (0)
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int num_1, num_2;
 	char *po;
 
 	if (argc > 4)
@@ -17,19 +17,19 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
+	num_1 = atoi(argv[1]);
 	po = argv[2];
-	num2 = atoi(argv[3]);
+	num_2 = atoi(argv[3]);
 	if ((get_op_func(po) == 0) || (po[1] != 0))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*po == '/' && num2 == 0) || (*po == '%' && num2 == 0))
+	if ((*po == '/' && num_2 == 0) || (*po == '%' && num_2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(po)(num1, num2));
+	printf("%d\n", get_op_func(po)(num_1, num_2));
 	return (0);
 }
