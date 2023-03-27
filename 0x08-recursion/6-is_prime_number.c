190ca6d5fd75d5654b2ef9668f5e4288e5fa7800
x-1 <x@g.co.za>
2023-03-27 03:04:28 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,25 +1,39 @@
 #include <stddef.h>
 #include "main.h"
+int _prime_(int, int);
 /**
- * is_prime_number - function that returns 1 if the input integer is
+ * is_prime_number - calling function that returns 1 if the input integer is
  * a prime number, otherwise return 0
- * @n: number to get square root of
- * Return: (1) || (0)
+ * @n: number
+ * Return: (_prime(n, 2))
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-	for (i = 2; i <= n / 2; i++)
+	return (_prime_(n, 2));
+}
+/**
+ * _prime_ - checks for prime number
+ * @n: number
+ * @i: iterator
+ * Return (0) || (1)
+ */
+int _prime_(int n, int i)
+{
+	if (n <= 2)
 	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
+		return (1);
 	}
-	return (1);
+	if ((n % i) == 0)
+	{
+		return (0);
+	}
+	if ((i * i) > n)
+	{
+		return (1);
+	}
+	return _prime_(n, i + 1);
 }
