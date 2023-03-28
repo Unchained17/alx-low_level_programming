4946171c7a6c95802c181e906585aea6f785a787
x-1 <x@g.co.za>
2023-03-28 08:07:46 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,12 +8,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
-	
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
+	int x;
+
+	for (x = 0; x < n && *(src + x) != '\0'; x++)
+		dest[x] = src[x];
+	for (; x < n; x++)
+		dest[x] = '\0';
+
 	return (dest);
 }
