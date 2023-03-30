cd7ce1eb8ae20a7cfc3098b9a02fcdf363910f69
x-1 <x@g.co.za>
2023-03-30 08:16:10 -0700

    2. take risks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,9 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, len = 0, len2 = 0, l;
 	char *p;
 
-	if (*s1 == 0)
+	if (s1 == NULL)
 		s1 = "";
-	if (*s2 == 0)
+	if (s2 == NULL)
 		s2 = "";
 	while (s1[i++] != '\0')
 		len += 1;
