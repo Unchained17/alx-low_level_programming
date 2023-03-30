814b999efb2cf826e534473c38c71c647329322d
x-1 <x@g.co.za>
2023-03-30 11:51:47 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 /**
  * print_list - function that prints all the elements of a list_t list.
  * @h: single linked list
@@ -7,8 +7,9 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t num = 0;
+	size_t num;
 
+	num = 0;
 	while (h != 0)
 	{
 		if ((*h).str == NULL)
