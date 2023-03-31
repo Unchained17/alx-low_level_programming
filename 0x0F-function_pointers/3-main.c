80b556b7244a0d4eb2964410789bc4525b74bc7d
x-1 <x@g.co.za>
2023-03-31 14:49:16 -0700

    A goal serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "function_pointers.h"
 #include "3-calc.h"
 #include <stdlib.h>
 /**
@@ -10,26 +11,26 @@
 int main(int argc, char *argv[])
 {
 	int num_1, num_2;
-	char *po;
+	char *p;
 
-	if (argc > 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 	num_1 = atoi(argv[1]);
-	po = argv[2];
+	p = argv[2];
 	num_2 = atoi(argv[3]);
-	if ((get_op_func(po) == 0) || (po[1] != 0))
+	if ((get_op_func(p) == 0) || (p[1] != 0))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*po == '/' && num_2 == 0) || (*po == '%' && num_2 == 0))
+	if ((*p == '/' && num_2 == 0) || (*p == '%' && num_2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(po)(num_1, num_2));
+	printf("%d\n", get_op_func(p)(num_1, num_2));
 	return (0);
 }
