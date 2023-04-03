f77f24bc98a613f527b39eeb748439eacaf449ba
x-1 <x@g.co.za>
2023-04-03 15:34:16 -0700

    2. One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%s", va_arg(arg, char *));
-		if (i < (n - 1) && *separator != '\0')
+		if (i != (n - 1) && *separator != '\0')
 			printf("%s", separator);
 	}
 	printf("\n");
